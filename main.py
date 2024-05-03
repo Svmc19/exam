10d24b99918e0f8f6fd81828f697d71137ebca84
Maftuna <svmca19@gmail.com>
2024-05-03 18:54:42 +0500

    exam

---
+++ main.py
@@ -0,0 +1,40 @@
+# def string():
+#     lsts = ["1","2","3.1"]
+#     lsts = int
+#     return lsts
+#
+# print(string())
+
+
+
+
+
+# def list(lst):
+#     lst1 = lst
+#     pop = lst.pop(-1)
+#     lst1.insert(0,pop)
+#     return lst1
+#
+#
+#
+# print(list([1,2,3,4,5]))
+
+#
+# def functiond(lst):
+#     for x in lst:
+#         for y in x:
+#             minss = min(y)
+#             return sum(minss)
+#
+#
+# print(functiond)
+# functiond(
+# [1,2,3,4,5,6],
+#     [5,6,7,8,9],
+#     [21,3,43,22,32]
+# )
+
+
+
+
+
